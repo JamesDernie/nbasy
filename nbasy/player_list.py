d2cfc2a91365fb544a099922eb0955ef9c5109cb
James Dernie <jamesdernie@gmail.com>
2018-09-26 13:59:29 +0800

    Update

--- nbasy/player_list.py
+++ nbasy/player_list.py
@@ -1,16 +1,15 @@
-from nbasy.utils import _get_json
 import nbasy.settings as settings
+from collections import OrderedDict
+from nbasy.utils import _get_json
 
 
 class PlayerList:
-    """
-
-    """
 
     _endpoint = "leaguedashplayerstats"
 
     def __init__(self,
-                 teams=None,  # Pass a list of teams
+                 teams=None,  # Pass a list of teams,
+                 players=None,  # Pass a list of players
                  player_position=settings.PlayerPosition.Default,
                  league_id=settings.NBA_LEAGUE_ID,
                  per_mode=settings.PerMode.PerGame,
@@ -36,9 +35,14 @@ class PlayerList:
                  outcome=settings.Outcome.Default,
                  game_segment=settings.GameSegment.Default,
                  period=settings.Period.AllQuarters,
-                 last_n_games=settings.LastNGames.Default):
+                 last_n_games=settings.LastNGames.Default,
+                 sorter=settings.Sorter.Default):
 
-        assert isinstance(teams, list), "teams must be a list type"
+        if teams:
+            assert isinstance(teams, list), "teams must be a list type"
+
+        if players:
+            assert isinstance(players, list), "players must be a list type"
 
         # Request params
         self.params = {
@@ -72,8 +76,8 @@ class PlayerList:
 
         # Filter Params
         self.teams = teams
-
-        self.get_player_list()
+        self.players = players
+        self.sorter = sorter
 
     def get_player_list(self):
 
@@ -87,20 +91,40 @@ class PlayerList:
 
     def convert_to_dict(self, headers, player_list):
         """
-        Converts the return results rowSet to a readable dict
+        Converts the return results rowSet to a readable ordered dict
         Format:
 
-            {
+            [(
                 player_name: {
                     attr: value,
                     attr: value,
+                    ...
                 }
-            }
+            )...]
         """
 
-        pass
+        # Get player name index in headers and remove from headers
+        i = headers.index("PLAYER_NAME")
+        headers.pop(i)
 
+        # Sort player list
+        sorter_index_key = headers.index(self.sorter)  # Need to know which item in list we want to sort by
+        sorted_player_list = sorted(player_list, key=lambda x: x[sorter_index_key], reverse=True)
 
+        player_dict = OrderedDict()
+        for player in sorted_player_list:
+
+            # Get player name for key
+            player_name = player.pop(i)
+
+            # Create key dict value
+            player_data = {}
+            for k, v in enumerate(player):
+                player_data[headers[k]] = v
+
+            player_dict[player_name] = player_data
+
+        return player_dict
 
     def process_json(self, json):
         """
@@ -120,12 +144,19 @@ class PlayerList:
             # Filter player list based on list of teams provided
             player_list = [player for player in player_list if player[i] in self.teams]
 
+        # Filter by player name
+        if self.players:
+
+            # Get index of 'PLAYER_NAME' in headers
+            i = headers.index('PLAYER_NAME')
+
+            # Convert players to lowercase
+            self.players = [x.lower() for x in self.players]
+
+            # Filter player list based on list of teams provided
+            player_list = [player for player in player_list if player[i].lower() in self.players]
+
         # Convert data to dict
         player_dict = self.convert_to_dict(headers, player_list)
 
         return player_dict
-
-        print(headers)
-        print(player_list)
-
-
---
+++ nbasy/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from .player_list import PlayerList
+
+
+class PlayerListTests(unittest.TestCase):
+    """
+    Tests for the player list class
+    """
+
+    def test_get_player_list_basic(self):
+        """
+        Test the get player list request, default params
+        """
+
+    def test_get_player_list_season(self):
+        pass
+
+    def test_get_player_list_teams(self):
+        pass
+
+    def test_get_player_list_players(self):
+        pass
